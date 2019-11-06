0ce0db93db6c4b504bcbb8435ee0f44e225f9ad9
wato18 <44283410+wato18@users.noreply.github.com>
2019-11-06 19:40:09 +0900

    update

--- 8章/8sho-71_80.py
+++ 8章/8sho-71_80.py
@@ -6,7 +6,6 @@ import networkx as nx
 import numpy as np
 
 
-
 # ノック７１：人間関係のネットワークを可視化してみよう
 df_links = pd.read_csv("links.csv")
 
@@ -68,15 +67,15 @@ def active_node_coloring(list_active):
     return list_color
 
 t = 0  # 最初
-nx.draw_networkx(G, font_color="w", node_color="active_node_coloring"(list_timeSeries[t]))
+nx.draw_networkx(G, font_color="w", node_color=active_node_coloring(list_timeSeries[t]))
 plt.show()  # 最初は一人
 
 t = 10  # 途中
-nx.draw_networkx(G, font_color="w", node_color="active_node_coloring"(list_timeSeries[t]))
+nx.draw_networkx(G, font_color="w", node_color=active_node_coloring(list_timeSeries[t]))
 plt.show()  # 徐々に増えていく（36か月くらいで全員に知れ渡る）
 
 t = 99  # 最後
-nx.draw_networkx(G, font_color="w", node_color="active_node_coloring"(list_timeSeries[t]))
+nx.draw_networkx(G, font_color="w", node_color=active_node_coloring(list_timeSeries[t]))
 plt.show()  # 最後は全員に知れ渡る
 
 
@@ -150,7 +149,7 @@ plt.show()  # 会員が減っていく様子が見られる
 # ノック７５：パラメータの全体像を，「相図」を見ながら把握しよう
 """相図：将来的に見てどうなっていくかという普及の様子を俯瞰する
 ここからの処理は計算時間を要する"""
-T_NUM = 100
+"""T_NUM = 100
 NUM_PhaseDiagram = 20
 phaseDiagram = np.zeros((NUM_PhaseDiagram, NUM_PhaseDiagram))  # 20 * 20
 for i_p in range(NUM_PhaseDiagram):  # 口コミ確率を変えていく，20
@@ -170,7 +169,7 @@ plt.xticks(np.arange(0.0, 20.0, 0.5), np.arange(0.0, 1.0, 0.25))  # (0.0, 20.0, 
 plt.yticks(np.arange(0.0, 20.0, 0.5), np.arange(0.0, 1.0, 0.25))
 plt.tick_params(bottom=False, left=False, right=False, top=False)
 plt.show()
-
+"""
 
 # ノック７６：実データを読み込んでみよう
 df_mem_links = pd.read_csv("links_members.csv")  # 540人のそれぞれのSNSでのつながりを表す
@@ -195,9 +194,91 @@ NUM = len(df_mem_info.index)  # 540人
 T_NUM = len(df_mem_info.columns) - 1  # 24カ月，0行目の顧客名のところを除く
 
 # 消滅の確率推定 #
+count_active = 0
+count_active_to_inactive = 0
+for t in range(1, T_NUM):
+    for i in range(NUM):
+        if (df_mem_info.iloc[i][t] == 1):  # その月ジムに通っているか
+            count_active_to_inactive += 1
+            if (df_mem_info.iloc[i][t + 1] == 0):  # 翌月は通っていない
+                count_active += 1
+estimated_percent_disapparence = count_active / count_active_to_inactive  # 活性（１）だったものが非活性（０）に変化した割合を数える
+print(estimated_percent_disapparence)
+
+# 拡散の確率推定 #
+count_link = 0
+count_link_to_active = 0
+count_link_temp = 0
+print(df_mem_info[str(3)] == 1)  # 各月tに対し，それぞれの人のTrue, Falseの列
+print(df_mem_info[df_mem_info[str(3)] == 1])  # Trueの人の行を取り出す
+for t in range(T_NUM - 1):  # 23ヶ月
+    df_link_t = df_mem_info[df_mem_info[str(t)] == 1]  # 各月tに対し，それぞれの人のTrue, Falseの列
+    temp_flag_count = np.zeros(NUM)  # 540人分のflag
+    for i in range(len(df_link_t.index)):  # その月tに対して，通っていた人の人数
+        df_link_temp = df_mem_links[df_mem_links["Node" + str(df_link_t.index[i])] == 1]  # 各人iに対し，それぞれの人のSNSでの繋がりを表す列，Trueの行を取り出す
+        for j in range(len(df_link_temp.index)):  # その人iとSNSで繋がっている人の人数
+            if (df_mem_info.iloc[df_link_temp.index[j]][t] == 0):  # iの友達jの期間tにおいて，ジムに通っていなかったら
+                if (temp_flag_count[df_link_temp.index[j]] == 0):  # その人のtemp_flag_countが立ってなかったら
+                    count_link += 1
+                if (df_mem_info.iloc[df_link_temp.index[j]][t + 1] == 1):  # iの友達jの期間t + 1において，ジムに通っていたら
+                    if (temp_flag_count[df_link_temp.index[j]] == 0):  # その人のtemp_flag_countが立ってなかったら
+                        temp_flag_count[df_link_temp.index[j]] = 1  # その人のtemp_flag_countを立てる，一回きり
+                        count_link_to_active += 1
+estimated_percent_percolation = count_link_to_active / count_link  # 非活性だったものが活性に変化した割合を数える
+print(estimated_percent_percolation)
 
 
-# ノック７９：
+# ノック７９：実データとシミュレーションを比較しよう
+percent_percolation = 0.02518466132375185
+percent_disapparence = 0.10147163541419416
+T_NUM = 24
+NUM = len(df_mem_links.index)  # 540人
+list_active = np.zeros(NUM)
+list_active[0] = 1
+list_timeSeries = []
+for t in range(T_NUM):
+    list_active = simulate_population(NUM, list_active, percent_percolation, percent_disapparence, df_mem_links)
+    list_timeSeries.append(list_active.copy())  # その期間での口コミが広まった人を配列の0,1を使って格納
+
+list_timeSeries_num = []
+for i in range(len(list_timeSeries)):
+    list_timeSeries_num.append(sum(list_timeSeries[i]))  # その期間でのジムに通っている人数を格納
+
+T_NUM = len(df_mem_info.columns) - 1
+list_timeSeries_num_real = []
+for t in range(0, T_NUM):
+    list_timeSeries_num_real.append(len(df_mem_info[df_mem_info[str(t)] == 1].index))  # 実際にその期間tのときにジムに通っている人数を格納
+
+plt.plot(list_timeSeries_num, label='simulated')
+plt.plot(list_timeSeries_num_real, label='real')
+plt.xlabel('month')
+plt.ylabel('population')
+# シミュレーションと現実にさほど差はない
 
 
-# ノック８０：
+# ノック８０：シミュレーションによる将来予測を実施しよう
+percent_percolation = 0.02518466132375185
+percent_disapparence = 0.10147163541419416
+T_NUM = 36  # 未来を予測する
+NUM = len(df_mem_links.index)  # 540人
+list_active = np.zeros(NUM)
+list_active[0] = 1
+list_timeSeries = []
+for t in range(T_NUM):
+    list_active = simulate_population(NUM, list_active, percent_percolation, percent_disapparence, df_mem_links)
+    list_timeSeries.append(list_active.copy())  # その期間での口コミが広まった人を配列の0,1を使って格納
+
+list_timeSeries_num = []
+for i in range(len(list_timeSeries)):
+    list_timeSeries_num.append(sum(list_timeSeries[i]))  # その期間でのジムに通っている人数を格納
+
+T_NUM = len(df_mem_info.columns) - 1
+list_timeSeries_num_real = []
+for t in range(0, T_NUM):
+    list_timeSeries_num_real.append(len(df_mem_info[df_mem_info[str(t)] == 1].index))  # 実際にその期間tのときにジムに通っている人数を格納
+
+plt.plot(list_timeSeries_num, label='simulated')
+plt.plot(list_timeSeries_num_real, label='real')
+plt.xlabel('month')
+plt.ylabel('population')
+# 急激な立下りが無く継続することが確認できる
---
+++ 9章/9sho-81_90.py
@@ -0,0 +1,35 @@
+# 9章 潜在顧客を把握するための画像認識１０本（ある程度の画像認識技術．実務重視，画像認識ライブラリOpenCV）
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from dateutil.relativedelta import relativedelta
+
+
+# ノック８１：画像データを読み込んでみよう
+
+
+# ノック８２：
+
+
+# ノック８３：
+
+
+# ノック８４：
+
+
+# ノック８５：
+
+
+# ノック８６：
+
+
+# ノック８７：
+
+
+# ノック８８：
+
+
+# ノック８９：
+
+
+# ノック９０：
