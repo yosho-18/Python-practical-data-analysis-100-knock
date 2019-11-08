27a5d84feb951ec3fbf620f7f11dd2df2758673f
wato18 <44283410+wato18@users.noreply.github.com>
2019-11-08 20:44:51 +0900

    update

--- 9章/9sho-81_90.py
+++ 9章/9sho-81_90.py
@@ -3,8 +3,40 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from dateutil.relativedelta import relativedelta
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import sin, cos
+from matplotlib.animation import FuncAnimation
+from scipy.integrate import odeint
+
+def func(state, t):
+    dydt = np.zeros_like(state)
+    dydt[0] = state[1]
+    dydt[1] = -(G / L) * sin(state[0])
+    return dydt
 
 
+G = 9.8  # 重力加速度
+L = 1  # 振り子の長さ
+
+th1 = 30.0  # 角度の初期値[deg]
+w1 = 0.0  # 角速度の初期値[deg]
+
+# 初期状態
+state = np.radians([th1, w1])
+dt = 0.05
+t = np.arange(0.0, 20, dt)
+
+sol = odeint(func, state, t)
+
+theta = sol[:, 0]
+x = L * sin(theta)
+y = - L * cos(theta)
+
+print([1, ] + ["w", "as"] + [2, ])
+print(np.random.randint(2 ** 30))
+print(np.arange(30) + 1)
 # ノック８１：画像データを読み込んでみよう
 
 
