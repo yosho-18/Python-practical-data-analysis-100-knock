a0b65baaaf548152884debe103e681ca5fed1484
yosho <44283410+yosho@users.noreply.github.com>
2019-11-10 23:25:51 +0900

    update

--- sho-1_0.py
+++ sho-1_0.py
@@ -1,11 +1,14 @@
 #
+import tensorflow as tf
 p = [[1,2,3], [9, 5]]
-print(p[:][])
+# print(p[:][])
 import pandas as pd
 import matplotlib.pyplot as plt
 from dateutil.relativedelta import relativedelta
-
-
+shape = [9]
+scale = 0.1
+initial = tf.random_uniform(shape, minval=-scale, maxval=scale, dtype=tf.float64)  #
+print(initial)
 # ノック１：
 
 
